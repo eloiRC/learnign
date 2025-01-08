ef26c3a35201ef79d6aa1df721d028e5ccb7d86a
eloi rebollo canela <39884899+eloiRC@users.noreply.github.com>
2025-01-08 23:39:35 +0100

    part 2 dia 1 node

---
+++ node desde cero/fs y async.js
@@ -0,0 +1,52 @@
+import { error, log } from "node:console"
+import fs from "node:fs"
+
+ const stats = fs.statSync('./archivo.txt')
+
+ console.log(
+    stats.isFile(),
+    stats.isDirectory(),
+    stats.isSymbolicLink(),
+    stats.size
+ );
+
+// !codigo syncrono
+
+// console.log('Leyendo primer archivo...');
+//  const text = fs.readFileSync("./archivo.txt", 'utf-8')
+
+// console.log(text);
+
+//  console.log('Leyendo segundo archivo...');
+//  const secondText = fs.readFileSync("./archivo2.txt", 'utf-8')
+
+//  console.log(secondText);
+
+
+ //! codi asyncrono
+ console.log('Leyendo primer archivo...');
+fs.readFile("./archivo.txt", 'utf-8',(err, text) => {
+    console.log(text);
+})
+
+console.log('hacer cosas mientras se ejecuta el codigo async');
+
+
+ console.log('Leyendo segundo archivo...');
+fs.readFile("./archivo2.txt", 'utf-8', (err, text) => {
+    console.log(text);
+})
+
+console.log('hacer mas cosas mientras se ejecuta el codigo async 2');
+
+
+// ! escito con promises
+// flataria anadir /promises al import
+// fs.readFile("./archivo2.txt", 'utf-8')
+//     .then( text =>{
+//         console.log(' texto por segunda vez con promises', text);
+//     }
+//     )
+
+
+
--- node desde cero/os-info.js
+++ node desde cero/os-info.js
@@ -1,13 +1,13 @@
-const os = require('node:os')
+import { platform, release, cpus, freemem, totalmem, uptime } from 'node:os'
 
 console.log('informacion del sistema operativo')
 console.log('---------------------------------')
 
-console.log('Nombre del OS: ', os.platform())
-console.log('Version del sistema operativo', os.release())
-console.log('CPUs',os.cpus())// podremos escalar procesos de node
-console.log('Memoria libre', os.freemem()/1024 /1024)
-console.log('Memoria total', os.totalmem()/1024/1024)
-console.log('uptime', os.uptime()/60/60)
+console.log('Nombre del OS: ', platform())
+console.log('Version del sistema operativo', release())
+console.log('CPUs',cpus())// podremos escalar procesos de node
+console.log('Memoria libre', freemem()/1024 /1024)
+console.log('Memoria total', totalmem()/1024/1024)
+console.log('uptime', uptime()/60/60)
 
 
