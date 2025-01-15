1352235b87b508b2171bf4fb80cb427ad675dad4
eloi rebollo canela <39884899+eloiRC@users.noreply.github.com>
2025-01-15 23:45:59 +0100

    express router dia 6

--- node desde cero/express video 2/router.mjs
+++ node desde cero/express video 2/router.mjs
@@ -1,9 +1,31 @@
 import http from 'node:http'
+import ditto from './pokemon/ditto'
 
 const processRequest = (req, res) => {
+  const { method, url } = req
 
+  switch (method) {
+    case 'GET':
+      switch (url) {
+        case 'pokemon-ditto':
+          res.setHeader('Content-Type', 'application/json; charset=utf-8')
+          return res.end(JSON.stringify(ditto))
+
+        default:
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end('<h1> Error 404</h1>')
+      }
+    case 'POST':
+      switch (url) {
+        case '/pokemon':
+          const body = ''
+          break
+
+        default:
+          break
+      }
+  }
 }
-
 const server = http.createServer(processRequest)
 
 server.listen(0, () => {
--- node desde cero/node video 1/fs-async-await paralell.js
+++ node desde cero/node video 1/fs-async-await paralell.js
@@ -1,18 +1,11 @@
-import { error, log } from "node:console"
-import fs from "node:fs/promises"
+import fs from 'node:fs/promises'
 
-//!codigo async await paralelo, mes rapid es llegeixen els dos arxius en paralel
+//! codigo async await paralelo, mes rapid es llegeixen els dos arxius en paralel
 
 Promise.all([
-    fs.readFile("./archivo.txt", 'utf-8'),
-    fs.readFile("./archivo2.txt", 'utf-8')
-]).then(([text,secondText])=>{
-    console.log(text);
-    console.log(secondText);
+  fs.readFile('./archivo.txt', 'utf-8'),
+  fs.readFile('./archivo2.txt', 'utf-8')
+]).then(([text, secondText]) => {
+  console.log(text)
+  console.log(secondText)
 })
-
-    
-
-
-
-
